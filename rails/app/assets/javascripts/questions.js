d6df734dfacd3a57fbd953fa389c1bbdd72b4f6b
Ishisaka, Tatsuya | Issy | MPD <tatsuya.ishisaka@mail.rakuten.com>
2016-02-14 01:26:53 +0900

    change haml to erb and coffee to js. add radio button

---
+++ rails/app/assets/javascripts/questions.js
@@ -0,0 +1,13 @@
+$(function (){
+  $("#ans_radio").hide();
+
+  $("#question_quiz_type").bind("change", function(){
+    if ($("#question_quiz_type").val() === '1') {
+      $("#ans_radio").show();
+      $("#ans_sent").hide();
+    } else if ($("#question_quiz_type").val() === '0') {
+      $("#ans_sent").show();
+      $("#ans_radio").hide();
+    }
+  });
+});
