e84d2ce4c50a7e9605faf882a131d2d871d6e91e
Ishisaka, Tatsuya | Issy | MPD <tatsuya.ishisaka@mail.rakuten.com>
2016-02-15 01:08:56 +0900

    mod layout and design bugfix

--- rails/app/assets/javascripts/questions.js
+++ rails/app/assets/javascripts/questions.js
@@ -1,12 +1,7 @@
 $(function (){
   $("#ans_radio").hide();
-  $("#question_quiz_type").bind("change", function(){
-    if ($("#question_quiz_type").val() === '1') {
-      $("#ans_radio").show();
-      $("#ans_sent").hide();
-    } else if ($("#question_quiz_type").val() === '0') {
-      $("#ans_sent").show();
-      $("#ans_radio").hide();
-    }
+  $("#question_quiz_type").on("change", function(){
+    $("#ans_radio").toggle();
+    $("#ans_sent").toggle();
   });
 });
